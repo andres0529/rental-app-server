0203b0a3e9323a98f43f3f602af0744af835de39
Andres Correa <andrescorrea.m@hotmail.com>
2023-03-31 17:31:38 -0400

    save data from file done

--- routes/runappfile.router.js
+++ routes/runappfile.router.js
@@ -2,7 +2,7 @@ const express = require("express");
 const runappfileService = require("./../services/runappfile.service");
 const router = express.Router();
 
-router.post("/", async(req, res) => {
+router.post("/", async (req, res) => {
   const result = await runappfileService(req.body);
   res.send(result);
 });
--- services/runappfile.service.js
+++ services/runappfile.service.js
@@ -1,6 +1,33 @@
+/* eslint-disable no-unused-vars */
+let InformationSchema = require("./../db/models/info.model.js");
+const connection = require("./../db/config.js");
+// let info = {
+//   address: "",
+//   dateCollected: "03/24/2023",
+//   municipality: "barrie",
+//   HousingType: "unlear",
+//   monthCollected: "march",
+//   unitSize: 9,
+//   qtyBathrooms: 9,
+//   secondarySuite: "unclear",
+//   typeSecondarySuite: "unclear",
+//   utilitiesIncluded: "Yes",
+//   totalCost: 999999,
+//   landlordType: "unclear",
+//   stability: "unclear",
+//   possibleDuplicate: "unclear",
+//   postCode: "",
+//   source: "Manual",
+// };
 
-const runappfileService = (data) => {
-  
+const runappfileService = async (data) => {
+  try {
+    const information = new InformationSchema(data);
+    await information.save();
+    return 200;
+  } catch (error) {
+    return error;
+  }
 };
 
 module.exports = runappfileService;
